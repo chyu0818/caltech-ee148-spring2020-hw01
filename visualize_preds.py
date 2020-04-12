e7d08fb2d674423c3116ee210ce85f9419a8850c
Christine Yu <christineyu@Christines-MacBook-Pro.local>
2020-04-11 23:37:16 -0700

    improved visualize

--- visualize_preds.py
+++ visualize_preds.py
@@ -1,18 +1,15 @@
 import json
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 import os
 
 def draw_boxes(I, bounding_boxes):
-    outline_rgb = [36, 248, 229]
-    (n_rows,n_cols,n_channels) = np.shape(I)
-    I_new = np.copy(I)
-
     # iterate through all boxes
-    for [tl_row,tl_col,br_row,br_col] in bounding_boxes:
-        I_new[tl_row:br_row+1,(tl_col,br_col),:] = outline_rgb
-        I_new[(tl_row,br_row),tl_col+1:br_col,:] = outline_rgb
-    return I_new
+    for box in bounding_boxes:
+        draw = ImageDraw.Draw(I)
+        draw.rectangle(box, outline=(36, 248, 229))
+        del draw
+    return I
 
 def main():
     # set the path to the downloaded data:
@@ -37,12 +34,8 @@ def main():
         # read image using PIL:
         I = Image.open(os.path.join(data_path,file_names[i]))
 
-        # convert to numpy array:
-        I = np.asarray(I)
-
-        I_new = draw_boxes(I, bounding_boxes[file_names[i]])
-        I_img = Image.fromarray(I_new)
-        I_img.save(os.path.join(preds_path,file_names[i]))
+        I = draw_boxes(I, bounding_boxes[file_names[i]])
+        I.save(os.path.join(preds_path,file_names[i]))
     return
 
 if __name__ == '__main__':
